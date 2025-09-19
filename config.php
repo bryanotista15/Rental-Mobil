f15ac28eafba2046e3693c2a38d86276ab011346
Natalino Marulitua Manuel Bryan <natalinomarulituamanuelbryan@gmail.com>
2025-09-19 09:52:53 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+if(!defined('BASEPATH')) echo "Tidak bisa langsung mengakses halaman ini!";
+
+define('DEFAULT_CONTROLLER', 'C_Auth');
+define('BASE_URL', 'http://rental-mobil.test/');
+define('APP_NAME', 'Aplikasi Rental Mobil');
+
+define('DB_HOST', '172.23.0.2');
+define('DB_USER', 'rental_mobil');
+define('DB_PASS', 'rental_mobil');
+define('DB_NAME', 'rental_mobil');
+
+?>
